0cdcaaa38a3fb41bfec6e7023bb011eaa1a1fc41
Phil <116698677+Jasmyre@users.noreply.github.com>
2023-02-13 22:20:22 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElemts = document.querySelectorAll(".hidden");
 hiddenElemts.forEach((el) => observer.observe(el));
+
+document.querySelector('.add-bot-input').addEventListener('change', function() {
+  var popUp = document.querySelector('.app .pop-up-add-bot-div');
+  
+  if (this.checked) {
+    popUp.style.display = 'block';
+    //document.querySelectorAll(".pop-up-inner-add-bot-div").style.display = 'block';
+    document.querySelectorAll(".pop-up-inner-add-bot-div").style.backgroundColor = "#0009"
+  } else {
+    popUp.style.display = 'none';
+    document.querySelectorAll(".pop-up-inner-add-bot-div").style.display = 'none';
+    document.querySelectorAll(".pop-up-inner-add-bot-div").style.backgroundColor = "#0000"
+  }
+});
